305977d60cf83fb2b978276d5a9263d827aefebc
David <l.david.dominguez.12@gmail.com>
2021-01-17 19:35:46 -0500

    primer cambio

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Luisa mamacita:',
+              'Luisa mamacita, te quiero:',
             ),
             Text(
               '$_counter',
